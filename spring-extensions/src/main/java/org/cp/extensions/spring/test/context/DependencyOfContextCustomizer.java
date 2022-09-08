0793678746c955c683e621ad4eb32519ce9358b4
John Blum <jblum@vmware.com>
2022-09-08 12:07:44 -0700

    Refactor DependencyOfContextCustomizer to be conditional on a property, enabled by default.

--- spring-extensions/src/main/java/org/cp/extensions/spring/test/context/DependencyOfContextCustomizer.java
+++ spring-extensions/src/main/java/org/cp/extensions/spring/test/context/DependencyOfContextCustomizer.java
@@ -15,10 +15,12 @@
  */
 package org.cp.extensions.spring.test.context;
 
+import org.cp.elements.lang.annotation.Nullable;
 import org.cp.extensions.spring.context.annotation.DependencyOf;
 import org.cp.extensions.spring.context.annotation.DependencyOfBeanFactoryPostProcessor;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.lang.NonNull;
 import org.springframework.test.context.ContextCustomizer;
 import org.springframework.test.context.MergedContextConfiguration;
@@ -26,31 +28,50 @@ import org.springframework.test.context.TestContext;
 
 /**
  * Spring Test {@link ContextCustomizer} used to customize the {@link ConfigurableApplicationContext} by registering
- * an instance of the {@link DependencyOfBeanFactoryPostProcessor}, thereby enabling {@link DependencyOf} annotation
- * declarations on beans defined in a Spring {@link TestContext}.
+ * (adding) an instance of the {@link DependencyOfBeanFactoryPostProcessor}, thereby enabling {@link DependencyOf}
+ * annotation declarations on beans defined and managed in a Spring {@link TestContext}.
  *
  * @author John Blum
+ * @see org.cp.extensions.spring.context.annotation.DependencyOf
  * @see org.cp.extensions.spring.context.annotation.DependencyOfBeanFactoryPostProcessor
  * @see org.springframework.context.ConfigurableApplicationContext
  * @see org.springframework.test.context.ContextCustomizer
+ * @see org.springframework.test.context.TestContext
  * @since 0.1.0
  */
 public class DependencyOfContextCustomizer implements ContextCustomizer {
 
+	public static final String TEST_CONTEXT_CUSTOMIZATION_ENABLED_PROPERTY =
+		"codeprimate.extensions.spring.test.context.customization-enabled";
+
+	protected static final boolean DEFAULT_TEST_CONTEXT_CUSTOMIZATION_ENABLED = true;
+
 	/**
-	 * Customizes the {@link ConfigurableApplicationContext} by registering an instance of
+	 * Customizes the {@link ConfigurableApplicationContext} by registering (adding) an instance of
 	 * the {@link DependencyOfBeanFactoryPostProcessor}.
 	 *
 	 * @param applicationContext {@link ConfigurableApplicationContext} to customize; must not be {@literal null}.
 	 * @param mergedConfig {@link MergedContextConfiguration} containing the merged configuration for the test.
 	 * @see org.cp.extensions.spring.context.annotation.DependencyOfBeanFactoryPostProcessor
-	 * @see org.springframework.test.context.MergedContextConfiguration
 	 * @see org.springframework.context.ConfigurableApplicationContext
+	 * @see org.springframework.test.context.MergedContextConfiguration
 	 */
 	@Override
 	public void customizeContext(@NonNull ConfigurableApplicationContext applicationContext,
 			@NonNull MergedContextConfiguration mergedConfig) {
 
-		DependencyOfBeanFactoryPostProcessor.registerWith(applicationContext);
+		if (isEnabled(applicationContext)) {
+			DependencyOfBeanFactoryPostProcessor.registerWith(applicationContext);
+		}
+	}
+
+	private boolean isEnabled(@Nullable ConfigurableApplicationContext applicationContext) {
+		return applicationContext != null && isEnabled(applicationContext.getEnvironment());
+	}
+
+	private boolean isEnabled(@Nullable Environment environment) {
+
+		return environment != null && environment.getProperty(TEST_CONTEXT_CUSTOMIZATION_ENABLED_PROPERTY, Boolean.TYPE,
+			DEFAULT_TEST_CONTEXT_CUSTOMIZATION_ENABLED);
 	}
 }
--- spring-extensions/src/test/java/org/cp/extensions/spring/test/context/DependencyOfContextCustomizerUnitTests.java
+++ spring-extensions/src/test/java/org/cp/extensions/spring/test/context/DependencyOfContextCustomizerUnitTests.java
@@ -15,43 +15,108 @@
  */
 package org.cp.extensions.spring.test.context;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-import org.junit.Test;
-
 import org.cp.extensions.spring.context.annotation.DependencyOfBeanFactoryPostProcessor;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.test.context.MergedContextConfiguration;
 
 /**
  * Unit Tests for {@link DependencyOfContextCustomizer}
  *
  * @author John Blum
- * @see org.junit.Test
+ * @see org.junit.jupiter.api.Test
  * @see org.mockito.Mockito
  * @see org.cp.extensions.spring.test.context.DependencyOfContextCustomizer
  * @see org.springframework.context.ConfigurableApplicationContext
+ * @see org.springframework.test.context.TestContext
  * @since 1.0.0
  */
 public class DependencyOfContextCustomizerUnitTests {
 
 	@Test
-	public void customizeApplicationContextCorrectly() {
+	public void customizesApplicationContextCorrectly() {
 
 		ConfigurableApplicationContext mockApplicationContext = mock(ConfigurableApplicationContext.class);
+
+		ConfigurableEnvironment mockEnvironment = mock(ConfigurableEnvironment.class);
+
+		doReturn(mockEnvironment).when(mockApplicationContext).getEnvironment();
+		doReturn(true).when(mockEnvironment)
+			.getProperty(eq(DependencyOfContextCustomizer.TEST_CONTEXT_CUSTOMIZATION_ENABLED_PROPERTY), eq(Boolean.TYPE),
+				eq(DependencyOfContextCustomizer.DEFAULT_TEST_CONTEXT_CUSTOMIZATION_ENABLED));
+
 		MergedContextConfiguration mockMergedContextConfiguration = mock(MergedContextConfiguration.class);
 
 		new DependencyOfContextCustomizer().customizeContext(mockApplicationContext, mockMergedContextConfiguration);
 
+		verify(mockApplicationContext, times(1)).getEnvironment();
+		verify(mockEnvironment, times(1))
+			.getProperty(eq(DependencyOfContextCustomizer.TEST_CONTEXT_CUSTOMIZATION_ENABLED_PROPERTY), eq(Boolean.TYPE),
+				eq(DependencyOfContextCustomizer.DEFAULT_TEST_CONTEXT_CUSTOMIZATION_ENABLED));
 		verify(mockApplicationContext, times(1))
 			.addBeanFactoryPostProcessor(isA(DependencyOfBeanFactoryPostProcessor.class));
+		verifyNoMoreInteractions(mockApplicationContext, mockEnvironment);
+		verifyNoInteractions(mockMergedContextConfiguration);
+	}
+
+	@Test
+	@SuppressWarnings("all")
+	public void customizeNullApplicationContextIsNullSafe() {
+		new DependencyOfContextCustomizer().customizeContext(null, mock(MergedContextConfiguration.class));
+	}
+
+	@Test
+	public void customizeApplicationContextWhenEnvironmentIsNull() {
+
+		ConfigurableApplicationContext mockApplicationContext = mock(ConfigurableApplicationContext.class);
+
+		doReturn(null).when(mockApplicationContext).getEnvironment();
+
+		MergedContextConfiguration mockMergedContextConfiguration = mock(MergedContextConfiguration.class);
+
+		new DependencyOfContextCustomizer().customizeContext(mockApplicationContext, mockMergedContextConfiguration);
+
+		verify(mockApplicationContext, times(1)).getEnvironment();
 		verifyNoMoreInteractions(mockApplicationContext);
+		verifyNoInteractions(mockMergedContextConfiguration);
+	}
+
+	@Test
+	public void doesNotCustomizeApplicationContextWithCustomizationIsDisabled() {
+
+		ConfigurableApplicationContext mockApplicationContext = mock(ConfigurableApplicationContext.class);
+
+		ConfigurableEnvironment mockEnvironment = mock(ConfigurableEnvironment.class);
+
+		doReturn(mockEnvironment).when(mockApplicationContext).getEnvironment();
+		doReturn(false).when(mockEnvironment)
+			.getProperty(eq(DependencyOfContextCustomizer.TEST_CONTEXT_CUSTOMIZATION_ENABLED_PROPERTY), eq(Boolean.TYPE),
+				eq(DependencyOfContextCustomizer.DEFAULT_TEST_CONTEXT_CUSTOMIZATION_ENABLED));
+
+		MergedContextConfiguration mockMergedContextConfiguration = mock(MergedContextConfiguration.class);
+
+		new DependencyOfContextCustomizer().customizeContext(mockApplicationContext, mockMergedContextConfiguration);
+
+		verify(mockApplicationContext, times(1)).getEnvironment();
+		verify(mockEnvironment, times(1))
+			.getProperty(eq(DependencyOfContextCustomizer.TEST_CONTEXT_CUSTOMIZATION_ENABLED_PROPERTY), eq(Boolean.TYPE),
+				eq(DependencyOfContextCustomizer.DEFAULT_TEST_CONTEXT_CUSTOMIZATION_ENABLED));
+		verify(mockApplicationContext, never()).addBeanFactoryPostProcessor(any(BeanFactoryPostProcessor.class));
+		verifyNoMoreInteractions(mockApplicationContext, mockEnvironment);
 		verifyNoInteractions(mockMergedContextConfiguration);
 	}
 }
