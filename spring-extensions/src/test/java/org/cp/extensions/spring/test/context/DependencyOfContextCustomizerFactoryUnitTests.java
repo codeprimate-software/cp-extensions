de1342da31078e7319fd10816ee556a3d188bfed
John Blum <jblum@vmware.com>
2022-09-08 12:11:52 -0700

    Switch to using the JUnit Jupiter @Test annotation in the DependencyOfContextCustomizerFactoryUnitTests class.

--- spring-extensions/src/test/java/org/cp/extensions/spring/test/context/DependencyOfContextCustomizerFactoryUnitTests.java
+++ spring-extensions/src/test/java/org/cp/extensions/spring/test/context/DependencyOfContextCustomizerFactoryUnitTests.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collections;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.test.context.ContextCustomizer;
 
